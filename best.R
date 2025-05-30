6975880c7cdff6d018348654830f5061804299ea
mo3tazJ <en.mo3taz@gmail.com>
2025-05-31 00:46:50 +0300

    building best function

--- best.R
+++ best.R
@@ -1,2 +1,9 @@
 ## Part 2
 ## Finding the best hospital in a state
+
+best <- function(state, outcome){
+    ## Read outcome data
+    ## Check that state and outcome are valid
+    ## Return hospital name in that state with lowest 30-day death
+    ## rate
+}
