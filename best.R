9b829a543cdeefbe180092d03481c56239868d8f
mo3tazJ <en.mo3taz@gmail.com>
2025-05-31 01:05:46 +0300

    Reading data and check arguments Added

--- best.R
+++ best.R
@@ -2,8 +2,21 @@
 ## Finding the best hospital in a state
 
 best <- function(state, outcome){
-    ## Read outcome data
-    ## Check that state and outcome are valid
+    # Read outcome data
+    data <- read.csv("outcome-of-care-measures.csv")
+    
+    # Normalizing the arguments (outcome and state)
+    outcome <- tolower(outcome)
+    choosen_state <- state
+    
+    # Check that state and outcome are valid
+    if(!chosen_state %in% unique(data[["State"]])){
+        stop("Invalid State")
+    }
+    if(!outcome %in% c("heart attack", "heart failure", "pneumonia")){
+        stop("Invalid Outcome")
+    }
+    
     ## Return hospital name in that state with lowest 30-day death
     ## rate
 }
