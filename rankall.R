c972804408aeb6f7c9a15937ed121e61441aa05b
mo3tazJ <en.mo3taz@gmail.com>
2025-06-01 02:34:58 +0300

    Finalizing the Function

--- rankall.R
+++ rankall.R
@@ -14,13 +14,38 @@ rankall <- function(outcome, num = "best") {
         stop("Invalid Outcome")
     }
     
+    # Renaming Considered Columns to be less verbose and lowercase
+    setnames(data
+             , tolower(sapply(colnames(data), gsub, pattern = "^Hospital 30-Day Death \\(Mortality\\) Rates from ", replacement = "" ))
+    )
+    
     # Columns indices to keep
     col_indices <- grep(paste0("hospital name|state|^",outcome), colnames(data))
     
+    # Filtering out unnecessary data 
+    out_dt <- data[, .SD ,.SDcols = col_indices]
     
+    # Find out what class each column is 
+    # sapply(fdata,class)
+    # Change outcome column class
+    out_dt[, outcome] <- out_dt[,  as.numeric(get(outcome))]
     
-    ## Check that state and outcome are valid
     ## For each state, find the hospital of the given rank
-    ## Return a data frame with the hospital names and the
-    ## (abbreviated) state name
+    if (num == "best"){
+        return(out_dt[order(state, get(outcome), `hospital name`)
+                      , .(hospital = head(`hospital name`, 1))
+                      , by = state])
+    }
+    
+    if (num == "worst"){
+        return(out_dt[order(get(outcome), `hospital name`)
+                      , .(hospital = tail(`hospital name`, 1))
+                      , by = state])
+    }
+    
+    ## Return a data frame with the hospital names and the (abbreviated) state name
+    return(out_dt[order(state, get(outcome), `hospital name`)
+                  , head(.SD,num)
+                  , by = state, .SDcols = c("hospital name") ])
+
 }
