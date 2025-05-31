f73e0f2fae41290c56369cd5dcc37663437c26ae
mo3tazJ <en.mo3taz@gmail.com>
2025-06-01 01:49:00 +0300

    Handling Read Data

--- rankhospital.R
+++ rankhospital.R
@@ -1,2 +1,14 @@
 ## Part 3
 ## Ranking hospitals by outcome in a state
+
+rankhospital <- function(state, outcome, num = "best") {
+    ## Read outcome data
+    data <- data.table::fread('outcome-of-care-measures.csv')
+    
+    ## Check that state and outcome are valid
+    
+    
+    ## Return hospital name in that state with the given rank 30-day death rate
+    
+    
+}
