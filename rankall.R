b3191eaae8c9727cf6a0f873b5eed041e443c1d4
mo3tazJ <en.mo3taz@gmail.com>
2025-06-01 02:08:36 +0300

    Reading data

--- rankall.R
+++ rankall.R
@@ -1,2 +1,17 @@
 ## Part 4
 ## Ranking hospitals in all states
+
+rankall <- function(outcome, num = "best") {
+    
+    ## Read outcome data
+    data <- data.table::fread('outcome-of-care-measures.csv')
+    
+    # Normalizing the argument (outcome)
+    outcome <- tolower(outcome)
+
+    
+    ## Check that state and outcome are valid
+    ## For each state, find the hospital of the given rank
+    ## Return a data frame with the hospital names and the
+    ## (abbreviated) state name
+}
